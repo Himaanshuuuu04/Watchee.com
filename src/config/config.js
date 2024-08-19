0bb715e21c3a8ad531f1add236f35aab0151f479
Himaansh_uu <himan0411singh@gmail.com>
2024-08-19 13:31:59 +0530

    initial commit

---
+++ src/config/config.js
@@ -0,0 +1,10 @@
+const config={
+    appwriteUrl:String(process.env.Appwrite_url),
+    appwriteProjectId:String(process.env.Appwrite_projectid),
+    appwriteBucketId:String(process.env.Appwrite_bucketid),
+    appwriteDatabaseId:String(process.env.Appwrite_databaseid),
+    appwriteCollectionId:String(process.env.Appwrite_collectionid)
+    
+
+}
+export default config;
