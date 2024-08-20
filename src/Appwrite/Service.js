e14fdbf91a37b9788c499baa2421ae8f1a88a6b1
Himaansh_uu <himan0411singh@gmail.com>
2024-08-20 20:59:00 +0530

    completed service.js

--- src/Appwrite/Service.js
+++ src/Appwrite/Service.js
@@ -1,26 +1,107 @@
-import { Client, Databases } from "appwrite";
+import { Client, Databases,Storage,ID ,Query} from "appwrite";
 import config from "../config/config";
 export class Service{
-    client=new client();
-    database;
-    bucket;
+    client=new Client();
+    databases;
+    storage;
     constructor(){
         this.client
         .setEndpoint(config.appwriteUrl)
         .setProject(config.appwriteProjectId);
-        this.database=new Databases(this.client);
-        this.bucket=new Bucket(this.client);
+        this.databases=new Databases(this.client);
+        this.storage=new Storage(this.client)
     }
+    async getPost(slug){
+        try {
+            return await this.databases.getDocument(config.appwriteDatabaseId,config.appwriteCollectionId,slug);
+        } catch (error) {
+            console.log("Appwrite service :: getPost() ", error);
+            return false;
+            
+        }
+
+    }
+    async getPosts(queries=[Query.equal("status","active")]){
+        try {
+            return await this.databases.listDocuments(config.appwriteDatabaseId,config.appwriteCollectionId,queries);
+        } catch (error) {
+            console.log("Appwrite service :: getPosts() :: ",error);
+            return false;
+        }
+    }
+
+    async createPost({title,slug,content,featuredImage,status,userId}){
+        try {
+            return await this.databases.createDocument(
+                config.appwriteDatabaseId,config.appwriteCollectionId,slug,
+                {title,content,featuredImage,status,userId});
+        } catch (error) {
+            console.log("Appwrite service :: CreatePost() :: ",error);
+            return false; 
+        }
+    }
+
+    async updatePost(slug,{title,content,featuredImage,status}){
+        try{
+            return await this.databases.updateDocument(
+                config.appwriteDatabaseId, // databaseId
+                config.appwriteCollectionId, // collectionId
+                slug, // documentId
+                {title,content,featuredImage,status}// data (optional)
+               
+            );
+        }catch(error){
+            console.log("Appwrite service :: updatePost() :: ",error);
+            return false;
+        }
+    }
+
+    async deletePost(slug){
+        try{
+            await this.databases.deleteDocument(
+                config.appwriteDatabaseId, // databaseId
+                config.appwriteCollectionId, // collectionId
+                slug // documentId               
+            );
+            return true;
+        }catch(error){
+            console.log("Appwrite service :: deletePost() :: ",error);
+            return false;
+        }
+    }
+
+    async uploadFile(file){
+        try {
+            return await this.storage.createFile(
+                config.appwriteBucketId, // bucketId
+                ID.unique() , // fileId
+                file
+            
+            );
+        } catch (error) {
+            console.log("Appwrite service :: uploadFile() :: ",error);
+            return false;
+        }
+    }
+
+    async deleteFile(fileId){
+        try {
+            return await this.storage.deleteFile(
+                config.appwriteBucketId, // bucketId
+                fileId // fileid
+            
+            );
+        } catch (error) {
+            console.log("Appwrite service :: deleteFile() :: ",error);
+            return false;
+        }
+    }
+    getPreview(fileId){
+        return this.storage.getFilePreview(
+            config.appwriteBucketId,fileId
+        ).href
+    }
+
 }
-const client = new Client()
-    .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
-    .setProject('&lt;YOUR_PROJECT_ID&gt;'); // Your project ID
-
-const databases = new Databases(client);
-const result = await databases.listDocuments(
-    '<DATABASE_ID>', // databaseId
-    '<COLLECTION_ID>', // collectionId
-    [] // queries (optional)
-);
-
-console.log(response);
+const service=new Service();
+export default service;
